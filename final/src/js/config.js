8f585dd6e1fab37929d5916c561b4d8f49fd1fcf
Rishav Raj <rajrishav011@gmail.com>
2024-12-03 23:03:03 +0530

    Add config file

---
+++ final/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ea38536e-92b6-4eb8-99d9-db686a1e4f7a';
+export const MODAL_CLOSE_SEC = 2.5;
